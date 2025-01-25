ed895fb14cd97c08952fc929829001775b0a2203
Omri H <omri.hermon@gmail.com>
2025-01-25 11:06:41 +0200

    oops

--- src/main/java/me/omrih/spoutjamplugin/Main.java
+++ src/main/java/me/omrih/spoutjamplugin/Main.java
@@ -17,7 +17,7 @@ public class Main extends JavaPlugin {
     public static Main plugin;
     public static Logger logger;
     public static final String blockPath = "https://raw.githubusercontent.com/Trioplane/SpoutJam-2025/refs/heads/main/textures/block/";
-    public static final String itemPath = "https://raw.githubusercontent.com/Trioplane/SpoutJam-2025/refs/heads/main/textures/block/";
+    public static final String itemPath = "https://raw.githubusercontent.com/Trioplane/SpoutJam-2025/refs/heads/main/textures/item/";
 
     public CondensedOakLeaves condensedOakLeaves;
     public CondensedBirchLeaves condensedBirchLeaves;
